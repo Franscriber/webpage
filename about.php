5592586066b19f12e703c3f42b36463572caa9de
Francis Quiambao <120738794+Franscriber@users.noreply.github.com>
2024-01-19 23:23:44 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="logo1.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CHUBS Kitchen | About Us</title>
+</head>
+<body>
+    <header>
+        <div class="header">
+
+            <div class="headerbar">
+                <div class="account">
+                    <ul>
+                        <a href="index.php">
+                            <li>
+                                <i class="fa-solid fa-user" id="user-mb"></i>
+                            </li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="nav">
+                    <ul>
+                        <a href="home.php">
+                            <li>HOME</li>
+                        </a>
+                        
+                        <a href="about.php">
+                            <li>ABOUT US</li>
+                        </a>
+
+                        <a href="food.php">
+                            <li>FOOD MENU</li>
+                        </a>
+
+                        <a href="index.php">
+                            <li>ORDER NOW</li>
+                        </a>
+                    </ul>
+                </div>
+            </div>
+            <div class="logo">
+                <img src="logo2.png" alt="">
+            </div>
+            <div class="bar">
+                <i class="fa-solid fa-bars"></i>
+                <i class="fa-solid fa-xmark" id="hdcross"></i>
+            </div>
+            <div class="nav">
+                <ul>
+                    <a href="home.php">
+                        <li>HOME</li>
+                    </a>
+                    <a href="about.php">
+                        <li>ABOUT US</li>
+                    </a>
+                    <a href="food.php">
+                        <li>FOOD MENU</li>
+                    </a>
+                    <a href="index.php">
+                        <li>ORDER NOW</li>
+                    </a>
+                </ul>
+            </div>
+            <div class="account">
+                <ul>
+
+                    <a href="index.php">
+                        <li>
+                            <i class="fa-solid fa-user" id="user-lap"></i>
+                        </li>
+                    </a>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <div class="home">
+        <div class="main_slide">
+            <div>
+                <h1><span>About</span>        Us</h1>
+                <p>CHUBS Kitchen, established in 2021, aims to provide high-quality, guaranteed products, particularly catering to spicy food enthusiasts. </br> We are 3rd Year Collage Students and we are given an order to create a website so we decided to use the business of my partner in this project. </br> The purpose of this porject is not just about our personal interest but we use this chance to develope our individual skills in both sides to business and school. </br> Furthermore, We are not assuming for a higher points or score but the learnings that we tried to understand to complete this Website Project.   </p>
+
+            </div>
+            <div>
+                <img src="about.png" alt="">
+            </div>
+        </div>
+
+        <div class="person-items-about">
+            <div class="person-about">
+                <div>
+                    <img src="reyna.png" alt="person">
+                </div>
+                <h3>Reyna Villegas</h3>
+                <p>CEO | Owner</p>
+            </div>
+            <div class="person-about">
+                <div>
+                    <img src="charls.png" alt="person">
+                </div>
+                <h3>Charls Pakingking</h3>
+                <p>Web Developer</p>
+            </div>
+            <div class="person-about">
+                <div>
+                    <img src="francis.png" alt="person">
+                </div>
+                <h3>Francis Quiambao</h3>
+                <p>Web Developer</p>
+            </div>
+        </div>
+
+        <div class="letter">
+            <div class="letter-head">
+                <h2>Subscribe <span>Newsletter</span></h2>
+            </div>
+            <div class="letter-input">
+                <div>
+                    <input type="email" placeholder="example@email.com">
+                </div>
+                <button class="red_button">Subscribe</button>
+            </div>
+        </div>
+    </div>
+    
+    <div class="footer">
+        <div class="footer-1">
+            <div class="logo">
+                <img src="logo2.png" alt="">
+            </div>
+            <div>
+                <address>
+                    <p>Email: reynavillegas@gmail.com</p>
+                    <p>Phone: 0963 492 9767</p>
+                    <p>Facebook: https://www.facebook.com/profile.php?id=61550265431274</p>
+                    <p>Topside Sisiman,</br> Mariveles, </br>Bataan ,</br> Philippines</p>
+                </address>
+            </div>
+        </div>
+
+        <div class="footer-2">
+            <img src="logo1.png" alt="">
+            <h2><em>CHUBS Kitchen</em></h2>
+        </div>
+
+    </div>
+
+    <script src="app.js"></script>
+</body>
+</html>
---
+++ food.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="logo1.png" type="image/x-icon">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>CHUBS Kitchen | Home</title>
+    </head>
+<body>
+    <header>
+        <div class="header">
+
+            <div class="headerbar">
+                <div class="account">
+                    <ul>
+                        <a href="index.php">
+                            <li>
+                                <i class="fa-solid fa-user" id="user-mb"></i>
+                            </li>
+                        </a>
+                            
+                    </ul>
+                </div>
+                <div class="nav">
+                    <ul>
+                        <a href="home.php">
+                            <li>HOME</li>
+                        </a>
+                        
+                        <a href="about.php">
+                            <li>ABOUT US</li>
+                        </a>
+
+                        <a href="food.php">
+                            <li>FOOD MENU</li>
+                        </a>
+
+                        <a href="index.php">
+                            <li>ORDER NOW</li>
+                        </a>
+                    </ul>
+                </div>
+            </div>
+            <div class="logo">
+                <img src="logo2.png" alt="">
+            </div>
+            <div class="bar">
+                <i class="fa-solid fa-bars"></i>
+                <i class="fa-solid fa-xmark" id="hdcross"></i>
+            </div>
+            <div class="nav">
+                <ul>
+                    <a href="home.php">
+                        <li>HOME</li>
+                    </a>
+                    <a href="about.php">
+                        <li>ABOUT US</li>
+                    </a>
+                    <a href="food.php">
+                        <li>FOOD MENU</li>
+                    </a>
+                    <a href="index.php">
+                        <li>ORDER NOW</li>
+                    </a>
+                </ul>
+            </div>
+            <div class="account">
+                <ul>
+                    <a href="index.php">
+                        <li>
+                            <i class="fa-solid fa-user" id="user-lap"></i>
+                        </li>
+                    </a>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <div class="home">
+        <div class="main_slide_food">
+            <h1>Food Menu</h1>
+        </div>
+
+        <div class="main_slide_food1">
+            <h1>Chili Products</h1>
+        </div>
+
+        <div class="food-items-food">
+            <div class="item-food">
+                <div>
+                    <img src="food1.jpg" alt="food item">
+                </div>
+                <h3>Sweet Chili Garlic Oil</h3>
+                <p>120 ML</p>
+                <button class="white_button">See Menu</button>
+            </div>
+            <div class="item-food">
+                <div>
+                    <img src="food2.jpg" alt="food item">
+                </div>
+                <h3>Sweet Chili Garlic Oil</h3>
+                <p>220 ML</p>
+                <button class="red_button">See Menu</button>
+            </div>
+            <div class="item-food">
+                <div>
+                    <img src="food3.jpg" alt="food item">
+                </div>
+                <h3>Bagoong Alamang</h3>
+                <p>120 ML</p>
+                <button class="white_button">See Menu</button>
+            </div>
+        </div>
+
+        <div class="main_slide_food2">
+            <h1>Dried Seafood Products</h1>
+        </div>
+        <div class="food-items-food2">
+            <div class="item-food">
+                <div>
+                    <img src="dry1.jpg" alt="food item">
+                </div>
+                <h3>Dried Alamang</h3>
+                <p>100 g</p>
+                <button class="white_button">See Menu</button>
+            </div>
+            <div class="item-food">
+                <div>
+                    <img src="dry2.jpg" alt="food item">
+                </div>
+                <h3>Dried Sapsap</h3>
+                <p>100 g</p>
+                <button class="red_button">See Menu</button>
+            </div>
+            <div class="item-food">
+                <div>
+                    <img src="dry3.jpg" alt="food item">
+                </div>
+                <h3>Dried Dilis</h3>
+                <p>100 g</p>
+                <button class="white_button">See Menu</button>
+            </div>
+            <div class="item-food">
+                <div>
+                    <img src="dry4.jpg" alt="food item">
+                </div>
+                <h3>Dried Espada</h3>
+                <p>100 g</p>
+                <button class="white_button">See Menu</button>
+            </div>
+            <div class="item-food">
+                <div>
+                    <img src="dry5.jpg" alt="food item">
+                </div>
+                <h3>Dried Bangus</h3>
+                <p>100 g</p>
+                <button class="white_button">See Menu</button>
+            </div>
+        </div>
+
+        <div class="letter">
+            <div class="letter-head">
+                <h2>Subscribe <span>Newsletter</span></h2>
+            </div>
+            <div class="letter-input">
+                <div>
+                    <input type="email" placeholder="example@email.com">
+                </div>
+                <button class="red_button">Subscribe</button>
+            </div>
+        </div>
+    </div>
+    
+    <div class="footer">
+        <div class="footer-1">
+            <div class="logo">
+                <img src="logo2.png" alt="">
+            </div>
+            <div>
+                <address>
+                    <p>Email: reynavillegas@gmail.com</p>
+                    <p>Phone: 0963 492 9767</p>
+                    <p>Facebook: https://www.facebook.com/profile.php?id=61550265431274</p>
+                    <p>Topside Sisiman,</br> Mariveles, </br>Bataan ,</br> Philippines</p>
+                </address>
+            </div>
+        </div>
+
+        <div class="footer-2">
+            <img src="logo1.png" alt="">
+            <h2><em>CHUBS Kitchen</em></h2>
+        </div>
+
+    </div>
+
+    <script src="app.js"></script>
+</body>
+</html>
